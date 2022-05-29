460ba91889d8b88055ecca26107700f9b55f9798
alirahmanicode <alirahmanicode@gmail.com>
2022-05-29 12:05:00 +0430

    change list display fields

--- user/admin.py
+++ user/admin.py
@@ -4,5 +4,6 @@ from .models import User
 
 class CustomUserAdmin(UserAdmin):
     model = User
+    list_display = ("username", "phone_number", "is_staff", "is_active_account")
 
 admin.site.register(User, CustomUserAdmin)
