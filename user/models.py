486c42d0254d6f97b91ee9ed5e04dc118e8ca15f
alirahmanicode <alirahmanicode@gmail.com>
2022-05-29 11:39:27 +0430

    add activate field to user model

--- user/models.py
+++ user/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=11)
+    is_active_account = models.BooleanField(default=False)
 
     REQUIRED_FIELDS = ["phone_number"]
