c190810b552f9542e5bdaccfd6537b6ba16ec89d
alirahmanicode <alirahmanicode@gmail.com>
2022-05-29 19:42:20 +0430

    create profile for new user by signlas

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Profile
 
 class CustomUserAdmin(UserAdmin):
     model = User
     list_display = ("username", "phone_number", "is_staff", "is_active_account")
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
