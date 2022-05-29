bf42172350f4b31f9fb0b6af9e6bd787f1410469
alirahmanicode <alirahmanicode@gmail.com>
2022-05-29 19:33:05 +0430

    create profile model

--- user/models.py
+++ user/models.py
@@ -6,3 +6,12 @@ class User(AbstractUser):
     is_active_account = models.BooleanField(default=False)
 
     REQUIRED_FIELDS = ["phone_number"]
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(blank=True, upload_to='profile image')
+    baio = models.TextField()
+    follower = models.ManyToManyField(User, related_name='followers')
+    following = models.ManyToManyField(User, related_name='following')
+    created = models.DateTimeField(auto_now_add=True)
+
