d40ea4fc7ea6868cf06919b125a6c08ad0cdd14b
alirahmanicode <alirahmanicode@gmail.com>
2022-06-03 22:32:01 +0430

    add comment for permission

--- post/permissions.py
+++ post/permissions.py
@@ -8,6 +8,7 @@ class CommentAccesse(permissions.BasePermission):
         return bool(request.user and request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
+        # just post owner and comment owner can delete that
         if request.method in self.safe_method:
             return True
         else:
